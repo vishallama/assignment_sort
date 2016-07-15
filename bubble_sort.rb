8e4d32ceee03ff443e4ac84c41bbd72fb68d7c26
David Watts <davidjeremywatts@gmail.com>
2016-07-15 09:28:33 -0700

    add work on merge_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,17 +2,20 @@
 
 def bubble_sort(array)
   array.length.times do
+    swaps = false
     (0..array.length - 2).each do |j|
       if array[j] > array[j + 1]
         temp = array[j]
         array[j] = array[j + 1]
         array[j + 1] = temp
+        swaps = true
       end
     end
+    return array if !swaps
   end
   array
 end
 
-bubble_sort( [1,3,7,2,5] ) # => [1, 2, 3, 5, 7]
-bubble_sort( [1] ) # => [1]
-bubble_sort( [] ) # => []
+p bubble_sort( [1,3,7,2,5] ) # => [1, 2, 3, 5, 7]
+p bubble_sort( [1] ) # => [1]
+p bubble_sort( [] ) # => []
