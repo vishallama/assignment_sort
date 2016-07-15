3cc061bb270f0f2a3d20acc0888b78deaf8d785d
Vishal Lama <vishal.lama@gmail.com>
2016-07-15 10:48:36 -0600

    Finish implementation of merge and merge_sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,9 +2,10 @@
 
 def merge_sort(array)
   return array if array.length < 2
-  i = array.length/2
+
+  i = (array.length / 2) - 1
   arr1 = merge_sort(array[0..i])
-  arr2 = merge_sort(array[i+1..-1])
+  arr2 = merge_sort(array[(i+1)..-1])
   merge(arr1, arr2)
 end
 
@@ -12,18 +13,36 @@ def merge(arr1, arr2)
   return_array = []
   i = 0
   j = 0
-  (arr1.length+arr2.length).times do
+
+  while i != arr1.length  && j != arr2.length
     if arr1[i] < arr2[j]
       return_array << arr1[i]
-      i += 1 unless i == arr1.length-1
+      i += 1
     else
       return_array << arr2[j]
-      j += 1 unless j ==arr2.length-1
+      j += 1
     end
   end
+
+  while i != arr1.length 
+    return_array << arr1[i]
+    i += 1
+  end
+
+  while j != arr2.length 
+    return_array << arr2[j]
+    j += 1
+  end
+
   return_array
 end
 
-p merge( [1,3,7],[2,5] ) # => [1, 2, 3, 5, 7]
-#p merge_sort( [1] ) # => [1]
-#p merge_sort( [] ) # => []
+# test merge
+merge( [],[] ) # => []
+merge( [1],[] ) # => [1]
+merge( [1,3,7],[2,5] ) # => [1, 2, 3, 5, 7]
+
+# test merge_test
+merge_sort( [1,3,7,2,5] ) # => [1, 2, 3, 5, 7]
+merge_sort( [1,3,7,2,5,10,-1,9,6] ) # => [-1, 1, 2, 3, 5, 6, 7, 9, 10]
+
